8ec7c895bda94084fc710fad14c8e0cc1f5ec419
sheyerdahl <97766761+sheyerdahl@users.noreply.github.com>
2023-10-09 18:19:37 -0700

    Made serviceWorker variable cleaner

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import App from "./containers/App";
 import reportWebVitals from './reportWebVitals';
 import "tachyons";
 import { searchCats, requestCats } from './reducers';
-import { register } from "./serviceWorkerRegistration"
+import * as serviceWorker from "./serviceWorkerRegistration"
 
 const logger = createLogger();
 
@@ -28,7 +28,7 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-register();
+serviceWorker.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
